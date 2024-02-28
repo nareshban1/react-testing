51d8ff30c74ed6eb4242ea3928add7642da9dcc9
Naresh Ban <nareshban40@gmail.com>
2024-02-28 16:34:05 +0545

    learning react testing library queries and  assertions

--- src/components/Text/Text.test.tsx
+++ src/components/Text/Text.test.tsx
@@ -1,8 +1,26 @@
 import { render, screen } from "@testing-library/react";
 import Text from "./Text";
 
-test("Text Renders Correctly", () => {
-  render(<Text text="Hello" />);
-  const textElement = screen.getByText(/Hello/i);
-  expect(textElement).toBeInTheDocument();
+// grouping tests together using describe
+describe("Component Tests", () => {
+  test("Component Renders Correctly", () => {
+    render(<Text text="Hello" />);
+    const textElement = screen.getByText(/Hello/i);
+    expect(textElement).toBeInTheDocument();
+
+    const inputElement = screen.getByRole("textbox", { name: "Name" });
+    expect(inputElement).toBeInTheDocument();
+
+    const inputElement2 = screen.getByLabelText("Name");
+    expect(inputElement2).toBeInTheDocument();
+
+    const selectElement = screen.getByRole("combobox");
+    expect(selectElement).toBeInTheDocument();
+
+    const textArea = screen.getByRole("textbox", { name: "Info" });
+    expect(textArea).toBeInTheDocument();
+
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Text = ({ text }: { text: string }) => {
-  return <div>{text}</div>;
+  return (
+    <div>
+      {text}
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+        <select>
+          <option value={"test"}>test</option>
+          <option value={"test1"}>test1</option>
+        </select>
+        <label htmlFor="info">Info</label>
+        <textarea name="info" id="info" />
+        <button>Button</button>
+      </form>
+    </div>
+  );
 };
 
 export default Text;
